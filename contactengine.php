08c5ab6a7f0ac862219969b8cb2bea9166f61429
Griff Hall <griffhall1@gmail.com>
2016-08-27 15:09:54 -0500

    trying new php form

---
+++ contactengine.php
@@ -0,0 +1,43 @@
+<?php
+
+$EmailFrom = "griffhall1@gmail.com";
+$EmailTo = "griffhall1@gmail.com";
+$Subject = "You have a new inquiry on your site:";
+$Name = Trim(stripslashes($_POST['name']));
+$Tel = Trim(stripslashes($_POST['phone']));
+$Email = Trim(stripslashes($_POST['email']));
+$Message = Trim(stripslashes($_POST['message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
