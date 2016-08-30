c82105132da378b1aab4ff5af370ced904433252
Griff Hall <griffhall1@gmail.com>
2016-08-29 23:25:13 -0500

    merge

--- js/init.js
+++ js/init.js
@@ -68,21 +68,22 @@
 		}
 	});
 
+	$(function() {
 
-var	$window = $(window),
-	$body = $('body');
+		var	$window = $(window),
+			$body = $('body');
 
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
 
-// Disable animations/transitions until the page has loaded.
-	$body.addClass('is-loading');
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
 
-	$window.on('load', function() {
-		$body.removeClass('is-loading');
+		// Touch mode.
+			if (skel.vars.isMobile)
+				$body.addClass('is-touch');
+
 	});
-
-// Touch mode.
-	if (skel.vars.isMobile)
-		$body.addClass('is-touch');
-
 
 })(jQuery);
