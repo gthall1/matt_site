c4238f53d6eaaf23fb677370e23bf4f718bb2acc
Griff Hall <griffhall1@gmail.com>
2016-08-31 15:08:29 -0500

    removed thank you standard

--- mail.php
+++ mail.php
@@ -9,8 +9,8 @@ $recipient = "griff@getluckee.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
 
+header("Location: thanks.html");
 
 ?>
 
