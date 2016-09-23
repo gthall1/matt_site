a9a7dde6179c2c0b63ffa6a6bceb01671856afe6
Griff Hall <griffhall1@gmail.com>
2016-09-23 12:07:21 -0500

    meta descriptions

--- mail.php
+++ mail.php
@@ -4,9 +4,9 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$formcontent="From: $name \n \n Email: $email \n \n Phone: $phone \n \n Message: $message";
-$recipient = "griff@getluckee.com";
-$subject = "Contact Form";
+$formcontent="From: $name \n Email: $email \n Phone: $phone \n Message: $message";
+$recipient = "matt@notohomebuilders.com";
+$subject = "Website Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 
